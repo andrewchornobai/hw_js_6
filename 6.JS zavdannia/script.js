d506c3472ac2a6b7075340d423bb847b7e90ca09
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:05:57 +0200

    added hw 6

---
+++ 6.JS zavdannia/script.js
@@ -0,0 +1,44 @@
+// zavdannia 1
+
+// let  arrow = (e) => {
+//     let a  = 0;
+//     let b = 1;
+//     let c;
+//     for(let i = 1;i<=e;i++){
+//         c = a + b;
+//         a = b;
+//         b = c;
+//         console.log(c);
+//     }
+// }
+// arrow(15);
+
+// zavdannia 2
+
+// function expression(a,b){
+//     let c = 1;
+//     console.log(c);
+//     for(let i = 0; i<a-1;i++){
+//         c *= b;
+//         console.log(c);
+//     }
+// }
+// expression(10,4);
+
+// zavdannia 3
+//     let pro1 = prompt('Write your first number');
+//     let pro2 = prompt('Write your second number');    
+//     let prompt1 = parseInt(pro1);    
+//     let prompt2 = parseInt(pro2);    
+//     number(prompt1,prompt2);
+
+// function number (a,b){
+//     let some_number = a;
+//     for(let i = a; i<b-1;i++){
+//         some_number += 1;
+//         console.log(some_number);
+//     }
+// }
+
+
+
